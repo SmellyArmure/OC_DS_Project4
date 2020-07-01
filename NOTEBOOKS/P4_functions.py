ce946d14cba7ead27e3aed5ad455c85bf4c7cd3a
Maryse <muller.maryse@gmail.com>
2020-07-01 18:40:08 +0200

    tests de normalité - tests de corrélation - encodage des variables catégorielles via un pipeline et test de plusieurs modèles... R2 maxi 0.28 git add --all!

---
+++ NOTEBOOKS/P4_functions.py
@@ -0,0 +1,400 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import missingno as msno
+import seaborn as sns
+
+from sklearn.decomposition import pca
+from sklearn.dummy import DummyRegressor, DummyClassifier
+from sklearn.feature_extraction import FeatureHasher
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV, KFold, StratifiedKFold, train_test_split
+from sklearn import metrics
+
+def move_cat_containing(my_index, strings, order):
+  idx_sel = []
+  if order == 'last':
+    index = my_index
+  elif order == 'first':
+    index = my_index[::-1]
+  else:
+    print("--- WARNING : index unchanged.\n -- Wrong order passed. Pass 'first' or 'last'")
+    return my_index
+  for s in strings:
+    idx_sel += [i for i,x in enumerate(index) if x in index[index.str.contains(s)]]
+  to_move = index[idx_sel]
+  rank = max(idx_sel)
+  mod_index = index.drop(to_move)
+  for c in to_move:
+    mod_index = mod_index.insert(rank,c)
+  return mod_index if order=='last' else mod_index[::-1]
+
+# Printing total nb and percentage of null
+
+def print_null_pct(df):
+    tot_null = df.isna().sum().sum()
+    print('nb of null: ', tot_null, '\npct of null: ',
+        '{:.1f}'.format(tot_null*100/(df.shape[0]*df.shape[1])))
+
+# Displaying number of missing values per column
+
+def plot_export_missing(df, cols, n_file, title,
+                        shorten_label=False, figsize=(12,8), save_enabled=False):
+    with plt.style.context('default'):
+        fig, axs = plt.subplots(2,1)
+        msno.matrix(df[cols] , sparkline=False,
+                    fontsize=11, ax=axs[0])
+        msno.bar(df[cols], ax=axs[1], fontsize=11)
+        if shorten_label:
+            for ax in axs:
+                lab = [item.get_text() for item in ax.get_xticklabels()]
+                short_lab = [s[:7]+'...'+s[-7:] if len(s)>14 else s for s in lab]
+                ax.axes.set_xticklabels(short_lab)
+    fig.set_size_inches(figsize)
+    [ax.grid() for ax in axs.flatten()];
+    [sns.despine(ax=ax, right=False, left=False,top=False, bottom=False)\
+                                        for ax in axs.flatten()];
+    plt.subplots_adjust(hspace=0.3)
+    plt.tight_layout(rect=[0, 0.05, 1, 0.92])
+    fig.suptitle(title, fontweight='bold', fontsize=14)
+    if not os.path.exists(os.getcwd()+'/FIG'):
+        os.makedirs('FIG')
+    if save_enabled: plt.savefig(os.getcwd()+'/FIG/'+n_file, dpi=400);
+    plt.show()
+
+# Plotting histograms of specified quantitative continuous columns of a dataframe and mean, median and mode values.
+
+def plot_histograms(df, cols, file_name=None, figsize=(12,7), layout=(3,3), save_enabled=False):
+
+    fig = plt.figure(figsize=figsize)
+
+    for i, c in enumerate(cols,1):
+        ax = fig.add_subplot(*layout,i)
+        ax.hist(df[c],  bins=30, color='grey')
+        ax.set_title(c)
+        ax.vlines(df[c].mean(), *ax.get_ylim(),  color='red', ls='-', lw=1.5)
+        ax.vlines(df[c].median(), *ax.get_ylim(), color='green', ls='-.', lw=1.5)
+        ax.vlines(df[c].mode()[0], *ax.get_ylim(), color='goldenrod', ls='--', lw=1.5)
+        ax.legend(['mean', 'median', 'mode'])
+        ax.title.set_fontweight('bold')
+        
+    plt.tight_layout(w_pad=0.5, h_pad=0.65)
+
+    if save_enabled: plt.savefig(os.getcwd()+'/FIG/'+file_name,
+                                dpi=400);
+    plt.show()
+
+# normality tests
+
+from scipy.stats import shapiro, normaltest, anderson
+
+def normality_tests(data, print_opt=False):
+    res_df = pd.DataFrame([])
+    data_notna = data[data.notna()]
+    # Shapiro-Wilk - D'Agostino's K^2
+    for f_name, func in zip(['Shapiro-Wilk', "D'Agostino K^2"],[shapiro, normaltest]):
+        stat, p = func(data_notna)
+        res_df.loc[f_name,'stat'] = stat
+        res_df.loc[f_name,'p_value'] = p
+        if print_opt: print('---'+f_name) 
+        if print_opt: print('stat=%.3f, p=%.3f' % (stat, p))
+        res_df.loc[f_name,'res'] = [p > 0.05]
+        if p > 0.05:
+            if print_opt: print('Probably Gaussian')
+        else:
+            if print_opt: print('Probably not Gaussian')
+    # Anderson-Darling
+    result = anderson(data_notna)
+    if print_opt: print('---'+'Anderson-Darling')
+    res_df.loc['Anderson-Darling','stat'] = result.statistic
+    if print_opt: print('stat=%.3f' % (result.statistic))
+    res_and = [(int(result.significance_level[i]),result.statistic < res)\
+                   for i,res in enumerate(result.critical_values)]
+    res_df.loc['Anderson-Darling','res'] = str(res_and)
+    for i in range(len(result.critical_values)):
+        sl, cv = result.significance_level[i], result.critical_values[i]
+        if result.statistic < cv:
+            if print_opt: print('Probably Gaussian at the %.1f%% level' % (sl))
+        else:
+            if print_opt: print('Probably not Gaussian at the %.1f%% level' % (sl))
+    return res_df
+
+
+# plotting histograms, qq plots and printing the results of normality tests 
+from statsmodels.graphics.gofplots import qqplot
+
+def plot_hist_qqplot(data, name, save=False):
+    fig, axs = plt.subplots(1,2)
+    # histogram
+    axs[0].hist(data, histtype='stepfilled',ec='k', color='lightgrey', bins =25);
+    # using statsmodels qqplot's module
+    qqplot(data, line='r', **{'markersize': 5, 'mec': 'k','color': 'lightgrey'}, ax=axs[1])
+    plt.gcf().set_size_inches(10,2.5)
+    fig.suptitle(name, fontweight='bold', size=14)
+    plt.tight_layout(rect=[0, 0.05, 1, 0.92])
+
+# Plotting bar plots of the main categorical columns
+
+def plot_barplots(df, cols, file_name=None, figsize=(12,7), layout=(2,3), save_enabled=False):
+
+    fig = plt.figure(figsize=figsize)
+    for i, c in enumerate(cols,1):
+        ax = fig.add_subplot(*layout,i)
+        ser = df[c].value_counts()
+        n_cat = ser.shape[0]
+        if n_cat>15:
+            ser[0:15].plot.bar(color='grey',ec='k', ax=ax)
+        else:
+            ser.plot.bar(color='grey',ec='k',ax=ax)
+        ax.set_title(c[0:17]+f' ({n_cat})', fontweight='bold')
+        labels = [item.get_text() for item in ax.get_xticklabels()]
+        short_labels = [s[0:7]+'.' if len(s)>7 else s for s in labels]
+        ax.axes.set_xticklabels(short_labels)
+        plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    if save_enabled: plt.savefig(os.getcwd()+'/FIG/'+file_name, dpi=400);
+    plt.show()
+
+
+# Testing for linear correlation (Pearson) and for monotonic relationship (Spearman, Kendall).
+
+from scipy.stats import pearsonr, spearmanr, kendalltau
+
+def correlation_tests(data1, data2, print_opt=False):
+    res_df = pd.DataFrame([])
+    # data1_notna = data1[data1.notna()]
+    # Pearson, Spearman, Kendall
+    for f_name, func in zip(['Pearson', 'Spearman', 'Kendall'],[pearsonr, spearmanr, kendalltau]):
+        stat, p = func(data1, data2)
+        res_df.loc[f_name,'stat'] = stat
+        res_df.loc[f_name,'p_value'] = p
+        if print_opt: print('---'+f_name)
+        if print_opt: print('stat=%.3f, p=%.3f' % (stat, p))
+        if print_opt: print('Probably independent') if p > 0.05 else print('Probably dependent')
+    return res_df
+
+################### KNN Imputation ########################
+
+def naive_model_compare_r2(X_tr, y_tr, X_te, y_te, y_pr):
+    # Model
+    print('--- model: {:.3}'.format(metrics.r2_score(y_te, y_pr)))
+    # normal random distribution
+    y_pr_rand = np.random.normal(0,1, y_pr.shape)
+    print('--- normal random distribution: {:.3}'\
+          .format(metrics.r2_score(y_te, y_pr_rand)))
+    # dummy regressors
+    for s in ['mean', 'median']:
+        dum = DummyRegressor(strategy=s).fit(X_tr, y_tr)
+        y_pr_dum = dum.predict(X_te)
+        print('--- dummy regressor ('+ s +') : r2_score={:.3}'\
+              .format(metrics.r2_score(y_te, y_pr_dum)))
+
+def naive_model_compare_acc_f1(X_tr, y_tr, X_te, y_te, y_pr, average='weighted'):
+    def f1_prec_recall(yte, ypr):
+        prec = metrics.precision_score(yte, ypr, average=average)
+        rec = metrics.recall_score(yte, ypr, average=average)
+        f1 = metrics.f1_score(yte, ypr, average=average)
+        return [f1, prec, rec]
+    # Model
+    print('--- model: f1={:.3}, precision={:.3}, recall={:.3}'\
+                                             .format(*f1_prec_recall(y_te, y_pr)))
+    # Dummy classifier
+    for s in ['stratified','most_frequent','uniform']:
+        dum = DummyClassifier(strategy=s).fit(X_tr, y_tr)
+        y_pr_dum = dum.predict(X_te)
+        print('--- dummy class. ('+ s\
+              +'): f1={:.3}, precision={:.3}, recall={:.3}'\
+                                             .format(*f1_prec_recall(y_te, y_pr_dum)))
+
+def plot_hist_pred_val(y_te, y_pr, y_pr_, bins=150, xlim=(0,20), short_lab=False):
+    # Plotting dispersion of data to be imputed
+    bins = plt.hist(y_te, alpha=0.5, color='b', bins=bins, density=True,
+                    histtype='step', lw=3, label='y_te (real val. from test set)')[1]
+    ax=plt.gca()
+    ax.hist(y_pr, alpha=0.5, color='g', bins=bins, density=True,
+            histtype='step', lw=3, label='y_pr (pred. val. from test set)');
+    ax.hist(y_pr_, alpha=0.5, color='r', bins=bins, density=True,
+            histtype='step', lw=3, label='y_pr_ (pred. val. to be imputed)');
+    ax.set(xlim=xlim)
+    plt.xticks(rotation=45, ha='right')
+    plt.draw()
+    if short_lab:
+        labels = [item.get_text() for item in ax.get_xticklabels()]
+        short_labels = [s[0:7]+'.' if len(s)>7 else s for s in labels]
+        ax.axes.set_xticklabels(short_labels)
+    ax.legend(loc=1)
+    plt.title("Frequency of values", fontweight='bold', fontsize=12)
+    plt.gcf().set_size_inches(6,2)
+    plt.show()
+
+# Data Preprocessing for quantitative and categorical data with encoding options
+def data_preprocessing(df, var_model, var_target, enc_strat_cat='label'):
+    ## Data Processing
+    df_train = df[var_model+[var_target]].copy('deep')
+    if df[var_model].isna().sum().sum()!=0 :
+        print("ERROR preprocessing: var_model columns should not contain nan !!!")
+        return None, None
+    else:
+        cat_cols = df_train[var_model].select_dtypes('object').columns
+        num_cols = df_train[var_model].select_dtypes(include=np.number).columns
+        # # Encoding categorical values
+        if enc_strat_cat == 'label':
+        # --- OPTION 1: Label Encoding categorical values
+            for c in cat_cols:
+                df_train[c] = LabelEncoder().fit_transform(df_train[c].values)
+        elif enc_strat_cat == 'hashing':
+        # --- OPTION 2: Feature hashing of categorical values
+            for c in cat_cols:
+                df_train[c] = df_train[c].astype('str')
+                n_feat = 5
+                hasher = FeatureHasher(n_features=n_feat, input_type='string')
+                f = hasher.transform(df_train[c])
+                arr = pd.DataFrame(f.toarray(), index=df_train.index)
+                df_train[[c+'_'+str(i+1) for i in range(n_feat)]] = pd.DataFrame(arr)
+                del df_train[c]
+                cols = list(df_train.columns)
+                cols.remove(var_target)
+                df_train = df_train.reindex(columns=cols+[var_target])
+        else:
+            print("ERROR: Wrong value of enc_strat_cat")
+            return None, None
+        # # Standardizing quantitative values
+        if len(list(num_cols)):
+            df_train[num_cols] = \
+                      StandardScaler().fit_transform(df_train[num_cols].values)
+        # Splitting in X and y, then in training and testing set
+        X = df_train.iloc[:,:-1].values
+        y = df_train.iloc[:,-1].values
+        return X, y
+
+# Data preprocessing, Knn training, then predicting all-in-one
+# inferring 'pnns1' (2_C) from .... + 'pnns2' (2_C)
+# Works for both quantitative (knnregressor)
+# and categorical (knnclassifier) target features
+
+def knn_impute(df, var_model, var_target, enc_strat_cat='label',
+                   clip=None, plot=True):
+    
+    if df[var_target].isna().sum()==0:
+        print('ERROR: Nothing to impute (target column already filled)')
+        return None, None
+    else :
+        if df[var_target].dtype =='object':
+            # knn classifier
+            print('ooo----KNN CLASSIFICATION :',var_target)
+            skf = StratifiedKFold(n_splits=5, shuffle=True)
+            gsCV = GridSearchCV(KNeighborsClassifier(),
+                            {'n_neighbors': [5,7,9,11,13]},
+                            cv=skf, return_train_score=True,
+                            scoring='f1_weighted')
+            mod = 'class'
+        elif df[var_target].dtype in ['float64', 'int64']:
+            # knn regressor
+            print('ooo----KNN REGRESSION :',var_target)
+            kf = KFold(n_splits=5, shuffle=True)
+            gsCV = GridSearchCV(KNeighborsRegressor(),
+                            {'n_neighbors': [3,5,7,9,11,13]},
+                            cv=kf, return_train_score=True)
+            mod = 'reg'
+        else:
+            print("ERROR: dtype of target feature unknown")
+        ## Data Preprocessing
+        X, y = data_preprocessing(df.dropna(subset=var_model+[var_target]),
+                                var_model=var_model, var_target=var_target,
+                                enc_strat_cat=enc_strat_cat)
+        X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.2)
+        ## Training KNN
+        gsCV.fit(X_tr, y_tr)
+        res = gsCV.cv_results_
+        ## Predicting test set with the model and clipping
+        y_pr = gsCV.predict(X_te)
+        try:
+            if clip: y_pr = y_pr.clip(*clip) # regressor option only
+        except:
+            print("ERROR: clip available for regressor option only") 
+        # Comparison with naive baselines
+        if mod == 'class':
+            naive_model_compare_acc_f1(X_tr,y_tr,X_te,y_te,y_pr,average='micro')
+        elif mod == 'reg':
+            naive_model_compare_r2(X_tr,y_tr,X_te,y_te,y_pr)
+        else:
+            print("ERROR: check type of target feature...")
+        ## Predicting using knn
+        ind_to_impute = df.loc[df[var_target].isna()].index 
+        X_, y_ = data_preprocessing(df.loc[ind_to_impute], var_model=var_model,
+                                    var_target=var_target,
+                                    enc_strat_cat=enc_strat_cat)
+        # Predicting with model
+        y_pr_ = gsCV.predict(X_)
+        # Plotting histogram of predicted values
+        short_lab = True if mod == 'class' else False
+        if plot: plot_hist_pred_val(y_te, y_pr, y_pr_, short_lab=short_lab)
+        # returning indexes to impute and calculated values
+        return ind_to_impute, y_pr_
+
+
+################### End of KNN Imputation ########################
+
+# Plotting heatmap (2 options available, rectangle or triangle )
+
+def plot_heatmap(corr, title, figsize=(8,4), vmin=-1, vmax=1, center=0,
+                 palette = sns.color_palette("coolwarm", 20), shape='rect',
+                 fmt='.2f', robust=False):
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    if shape == 'rect':
+        mask=None
+    elif shape == 'tri':
+        mask = np.zeros_like(corr, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+    else:
+        print('ERROR : this type of heatmap does not exist')
+
+    palette = palette
+    ax = sns.heatmap(corr, mask=mask, cmap=palette, vmin=vmin, vmax=vmax,
+                     center=center, annot=True, annot_kws={"size": 10},fmt=fmt,
+                     square=False, linewidths=.5, linecolor = 'white',
+                     cbar_kws={"shrink": .9, 'label': None}, robust = robust,
+                     xticklabels= corr.columns, yticklabels = corr.index)
+    ax.tick_params(labelsize=10,top=False, bottom=True,
+                labeltop=False, labelbottom=True)
+    ax.collections[0].colorbar.ax.tick_params(labelsize=10)
+    plt.setp(ax.get_xticklabels(), rotation=25, ha="right",rotation_mode="anchor")
+    ax.set_title(title, fontweight='bold', fontsize=12)
+
+
+# Plotting explained variance ratio in scree plot
+
+def scree_plot(col_names, exp_var_rat, ylim=(0,0.4)):
+    plt.bar(x=col_names, height=exp_var_rat, color='grey')
+    ax1 = plt.gca()
+    ax1.set(ylim=ylim)
+    ax2 = ax1.twinx()
+    ax2.plot(exp_var_rat.cumsum(), 'ro-')
+    ax2.set(ylim=(0,1.1))
+    ax1.set_ylabel('explained var. rat.')
+    ax2.set_ylabel('cumulative explained var. rat.')
+
+    for i, p in enumerate(ax1.patches):
+        ax1.text( p.get_width()/5 + p.get_x(), p.get_height()+ p.get_y()+0.01,
+                '{:.0f}%'.format(exp_var_rat[i]*100),
+                    fontsize=8, color='k')
+        
+    plt.gcf().set_size_inches(8,3)
+    plt.title('Scree plot', fontweight='bold')
+
+
+# Returing main regressor scores
+
+def scores(name, Xte, yte, ypr):
+    MAE = metrics.mean_absolute_error(yte, ypr)
+    MSE = metrics.mean_squared_error(yte, ypr)
+    RMSE = np.sqrt(MSE)
+    RMSLE = np.sqrt(np.mean((np.log(ypr+1) - np.log(yte+1))**2))
+    R2 = metrics.r2_score(yte, ypr)
+    return pd.Series([MAE, MSE, RMSE, RMSLE, R2],
+                     index = ['MAE', 'MSE', 'RMSE', 'RMSLE', 'R2'],
+                     name=name)
+
